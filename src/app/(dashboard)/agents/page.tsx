18cac23b3993c5bcb7bef6dba8ce4e588f10b30e
piggy-devil <muupaza@gmail.com>
2025-06-06 10:07:54 +0700

    09: agents setup

---
+++ src/app/(dashboard)/agents/page.tsx
@@ -0,0 +1,26 @@
+import {
+  AgentsView,
+  AgentsViewError,
+  AgentsViewLoading,
+} from "@/modules/agents/ui/views/agents-views";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+
+const AgentsPage = () => {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<AgentsViewLoading />}>
+        <ErrorBoundary fallback={<AgentsViewError />}>
+          <AgentsView />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrationBoundary>
+  );
+};
+
+export default AgentsPage;
