cdb07965730175ae0bacbf910b49144969135401
piggy-devil <muupaza@gmail.com>
2025-06-07 10:58:06 +0700

    14:agent page

---
+++ src/app/(dashboard)/agents/[agentId]/page.tsx
@@ -0,0 +1,34 @@
+import {
+  AgentIdView,
+  AgentsIdViewError,
+  AgentsIdViewLoading,
+} from "@/modules/agents/ui/views/agent-id-view";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+interface AgentIdPageProps {
+  params: Promise<{ agentId: string }>;
+}
+
+const AgentIdPage = async ({ params }: AgentIdPageProps) => {
+  const { agentId } = await params;
+
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(
+    trpc.agents.getOne.queryOptions({ id: agentId })
+  );
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<AgentsIdViewLoading />}>
+        <ErrorBoundary fallback={<AgentsIdViewError />}>
+          <AgentIdView agentId={agentId} />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrationBoundary>
+  );
+};
+
+export default AgentIdPage;
